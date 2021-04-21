219fbd08b738acb95168c8efdcab376542ce7aea
Jason Van Cleave <jvcleave@gmail.com>
2021-04-21 15:01:43 -0400

    test

--- Example/jvcLib/ViewController.swift
+++ Example/jvcLib/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import jvcLib
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let utils = JVCUtils();
+        utils.test();
     }
 
     override func didReceiveMemoryWarning() {
---
+++ jvcLib/Classes/JVCUtils.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+
+public class JVCUtils
+{
+    public init() {}
+    
+    public func test()
+    {
+        
+        print("JVCUtils hello!")
+        
+    }
+}
