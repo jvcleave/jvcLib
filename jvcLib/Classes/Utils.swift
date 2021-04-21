8c220483a73d28dca12c6bd7b0a59c1290dc1652
Jason Van Cleave <jvcleave@gmail.com>
2021-04-21 14:52:31 -0400

    tst

---
+++ jvcLib/Classes/Utils.swift
@@ -0,0 +1,19 @@
+//
+//  Utils.swift
+//  jvcLib
+//
+//  Created by jason van cleave on 4/21/21.
+//
+
+import Foundation
+
+public class Utils
+{
+    
+    public func test()
+    {
+        
+        print("Utils hello!")
+
+    }
+}
