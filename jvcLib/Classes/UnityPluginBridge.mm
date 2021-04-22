18afc60d2043095c4c4e28c2dcc2a93bb0f98332
Jason Van Cleave <jvcleave@gmail.com>
2021-04-22 12:24:39 -0400

    header

---
+++ jvcLib/Classes/UnityPluginBridge.mm
@@ -0,0 +1,36 @@
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface UnityPlugin : NSObject
++ (UnityPlugin * _Nonnull)shared;
+- (NSInteger)addWithA:(NSInteger)a b:(NSInteger)b ;
+- (NSInteger)retrieveRecordCount ;
+- (nonnull instancetype)init ;
+@end
+
+@implementation UnityPlugin
+
+
+
+
+@end
+
+extern "C" {
+
+#pragma mark - Functions
+
+int addNumbers(int a , int b)
+{
+    
+    int result = [[UnityPlugin shared] addWithA:a b:b];
+    return result;
+}
+
+int getRecordCount()
+{
+    int result = [[UnityPlugin shared] retrieveRecordCount];
+    return result;
+}
+
+}
